ac12cbe0934e8f1ecc69a3e010c1fde6289c75e4
Data-Anal-Ojisan <31760481+Ryosuke-HIRAMTO@users.noreply.github.com>
2020-09-06 23:18:17 +0900

    update

--- python_samples/stan/plotSSM.py
+++ python_samples/stan/plotSSM.py
@@ -20,12 +20,11 @@ def plotSSM(mcmc_sample, time_vec, obs_vec, state_name,
     #   state_name  : 図示する状態の変数名
     #   graph_title : グラフタイトル
     #   y_label     : y軸のラベル
-    #   date_labels : 日付の書式
     #
     # Returns:
     #   生成されたグラフ
     
-    ## すべての時点の状態の、95%区間と中央値
+    # すべての時点の状態の、95%区間と中央値
     result_df = pd.DataFrame(np.zeros([mcmc_sample[state_name].shape[1], 3]))
     for i in range(mcmc_sample[state_name].shape[1]):
         result_df.iloc[i,:] = np.percentile(mcmc_sample[state_name][:,i], q=[2.5, 50, 97.5])
@@ -63,6 +62,3 @@ def plotSSM(mcmc_sample, time_vec, obs_vec, state_name,
         
     # グラフを返す
     plt.show()
-    
-    
-
