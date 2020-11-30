4f030a0441992a3ff3d0270d5c87fc9ac925a640
Data-Anal-Ojisan <31760481+Ryosuke-HIRAMTO@users.noreply.github.com>
2020-11-30 23:07:17 +0900

    update root setting

--- python_samples/tkinter/00_root_window.py
+++ python_samples/tkinter/00_root_window.py
@@ -5,7 +5,45 @@ Created on Thu Jul 30 19:30:51 2020
 @author: data-anal-ojisan
 """
 
-import tkinter as ttk
+import tkinter as tk
 
-root = ttk.Tk()
+# # 設定なし
+# root = tk.Tk()
+# root.mainloop()
+
+# # タイトル変更
+# root = tk.Tk()
+# root.title('SampleApp')
+# root.mainloop()
+#
+# # サイズ調整
+# root = tk.Tk()
+# root.geometry('600x600')
+# root.mainloop()
+#
+# # サイズ固定
+# root = tk.Tk()
+# root.resizable(height=False, width=False)
+# root.mainloop()
+#
+# # 最小サイズ変更
+# root = tk.Tk()
+# root.minsize(height=300, width=200)
+# root.mainloop()
+#
+# # 最大サイズ変更
+# root = tk.Tk()
+# root.maxsize(height=300, width=200)
+# root.mainloop()
+#
+# # アイコン変更
+# root = tk.Tk()
+# root.iconbitmap(default='favicon.ico')
+# root.mainloop()
+
+# 背景色変更
+root = tk.Tk()
+root.configure(background='#000000000')
 root.mainloop()
+
+
